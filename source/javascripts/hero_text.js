9bac560883509c12ccc6e3c24d39f314014335d5
marinaaisa <marina@marinaaisa.com>
2017-06-30 23:13:35 +0200

    [HTML + CSS + JS] Added hero to home page

---
+++ source/javascripts/hero_text.js
@@ -0,0 +1,21 @@
+var theater = theaterJS()
+
+theater
+  .on('type:start, erase:start', function () {
+    theater.getCurrentActor().$element.classList.add('hero-text--typing')
+  })
+  .on('type:end, erase:end', function () {
+    theater.getCurrentActor().$element.classList.remove('hero-text--typing')
+  })
+  .on('type:start, erase:start', function () {
+    if (theater.getCurrentActor().name === 'hero-text') {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  })
+
+theater
+  .addActor('hero-text', { speed: .8, accuracy: 1 })
+  .addScene('hero-text:Hi! 👋 I\'m Marina Aisa', 600, ', <br> a product designer.', 600, function (done) { document.getElementById("hero-text").style.background = 'black'; done(); }, -17, 'front-end developer.', 600,function (done) { document.getElementById("hero-text").style.background = 'white'; done(); })
+  .addScene(theater.replay.bind(theater))
