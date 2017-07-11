3dbb19e7d12af30040dcc8fff4fe53cc3a5a1038
Marina Aisa <marina@marinaaisa.com>
2017-07-11 12:13:15 +0200

    Floating shapes

---
+++ source/javascripts/body/floating-shapes.js
@@ -0,0 +1,10 @@
+(function() {
+  $(".fa").each( function() {
+    var randLeft = Math.floor(Math.random() * 90) + 1;
+    var randfontSize = Math.floor(Math.random() * 3) + 8;
+    var randRotation = Math.floor(Math.random() * 360) + 1;
+    var randDelay = Math.floor(Math.random() * 5) + 1;
+    var randDuration = Math.floor(Math.random() * 10) + 5;
+    $(this).attr("style",("left:" + randLeft + "vw; font-size:" + randfontSize + "vw; transform: rotate(" + randRotation +"deg); animation-delay: " + randDelay + "s; animation-duration: " + randDuration + "s;)"));
+  });
+})();
