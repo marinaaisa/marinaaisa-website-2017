2e49cc798d86f7738629bb6bf15501e408bba12d
Marina Aisa <marina@marinaaisa.com>
2017-07-11 14:12:44 +0200

    Floating shapes finished

--- source/javascripts/body/floating-shapes.js
+++ source/javascripts/body/floating-shapes.js
@@ -1,6 +1,6 @@
 (function() {
   $(".fa").each( function() {
-    var randLeft = Math.floor(Math.random() * 90) + 1;
+    var randLeft = Math.floor(Math.random() * 9) + 1;
     var randfontSize = Math.floor(Math.random() * 3) + 8;
     var randRotation = Math.floor(Math.random() * 360) + 1;
     var randDelay = Math.floor(Math.random() * 5) + 1;
