25e4599f55e2c9232898d0f6f3463c681c15473d
Marina Aisa <marina@marinaaisa.com>
2017-07-12 16:38:48 +0200

    Adding footer (not finished)

--- source/javascripts/body/floating-shapes.js
+++ source/javascripts/body/floating-shapes.js
@@ -1,5 +1,5 @@
 (function() {
-  $(".fa").each( function() {
+  $(".floating").each( function() {
     var randLeft = Math.floor(Math.random() * 9) + 1;
     var randfontSize = Math.floor(Math.random() * 3) + 8;
     var randRotation = Math.floor(Math.random() * 360) + 1;
