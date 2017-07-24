06d4a1cd12aae69b42ff20688a93de4da036a518
marinaaisa <marina@marinaaisa.com>
2017-07-25 01:21:51 +0200

    Fixing bugs

--- source/javascripts/hero_text.js
+++ source/javascripts/hero_text.js
@@ -17,5 +17,5 @@ theater
 
 theater
   .addActor('hero-text', { speed: .8, accuracy: 1 })
-  .addScene('hero-text:Hi! <any emoji="waving-hand" emoji-tone="light"></any> I\'m <a href="#">Marina Aisa</a>', 600, ', <br> a product designer.', 600, function (done) { document.getElementById("hero-text").style.background = 'black'; done(); }, -17, 'front-end developer.', 600,function (done) { document.getElementById("hero-text").style.background = 'white'; done(); })
+  .addScene('hero-text:Hi! <any emoji="waving-hand" emoji-tone="light"></any> I\'m <a target="_blank" href="https://twitter.com/MarinaAisa">Marina Aisa</a>', 600, ', <br> a product designer.', 600, function (done) { document.getElementById("hero-text").style.background = 'black'; done(); }, -17, 'front-end developer.', 600,function (done) { document.getElementById("hero-text").style.background = 'white'; done(); })
   .addScene(theater.replay.bind(theater))
